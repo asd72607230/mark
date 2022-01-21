2788d78a1e31d4a527057f7a9ee08eca2666d3cb
CHEN <asd72607230@gmail.com>
2022-01-21 09:53:23 +0800

    01210953

---
+++ test1/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const bcrypt =require('bcrypt');
+const passport = require('passport');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    
+})
+
+app.listen(3000)
