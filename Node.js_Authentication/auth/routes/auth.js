22634b10f178ef4e95a90f5a0f8a98e94a3af815
Chen <asd72607230@gmail.com>
2022-03-15 13:42:09 +0800

    emailCheck

--- Node.js_Authentication/auth/routes/auth.js
+++ Node.js_Authentication/auth/routes/auth.js
@@ -1,31 +1,27 @@
 const router = require("express").Router();
 const User = require("../module/User");
-
-//Validation
-const Joi = require("@hapi/joi");
-
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-});
+const { registerValidation, loginValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
+    const {error} = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-    const validation = schema.validate(req.body);
-    res.send(validation);
+    //檢查信箱是否重複
+    const emailExist = User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send("Email already exists");
 
-    // const user = new User({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password
-    // });
-    // try {
-    //     const savedUser = await user.save();
-    //     res.send(savedUser);
-    // } catch (err) {
-    //     res.status(400).send(err)
-    // }
+    // Create a new user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (err) {
+        res.status(400).send(err)
+    }
 })
 
 module.exports = router;
